06633bc10ac1d49ce46ecf298961e3bedb46e26c
prachikh <pravin.chikhale@publicissapient.com>
2019-08-14 10:47:45 +0530

    New changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           This is fun!! :)
+          Checking git push
         </p>
         <a
           className="App-link"
